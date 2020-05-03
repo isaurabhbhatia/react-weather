ebcf72d9095b9c137a94ce0882806435011b67ba
Saurabh Bhatia <saurabh.bhatia@ziploan.in>
2020-05-03 22:15:13 +0530

    Adding styles and classes to the Link and IndexLink via inbuild attributes of Link, IndexLink

---
+++ app/components/Weather.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Weather = React.createClass({
+  render: function (){
+    return (
+      <div>
+        <h2>Weather Component</h2>
+      </div>
+    )
+  }
+});
+
+module.exports = Weather;
