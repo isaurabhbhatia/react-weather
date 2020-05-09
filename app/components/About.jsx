4f95539f61804f116e26fcc2713077194d1dfe98
Saurabh Bhatia <saurabh.bhatia@ziploan.in>
2020-05-09 14:24:00 +0530

    Updating About.jsx

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,13 +1,12 @@
 var React = require('react');
 
-var About = React.createClass({
-  render: function (){
-    return (
-      <div>
-        <h2>About Component</h2>
-      </div>
-    )
-  }
-});
+var About = (props) => {
+  return (
+    <div>
+      <h2>About</h2>
+      <p>Welcome to About page.</p>
+    </div>
+  )
+};
 
 module.exports = About;
